b37b81d1c5bcdcf72736fa3795f2c9d68b4bec81
tarunkanagala123 <tarun.kanagala@zemosolabs.com>
2024-10-22 20:05:44 +0530

    Added Homepage

---
+++ front_end/src/Components/Homepage.tsx
@@ -0,0 +1,8 @@
+import { Typography } from "@mui/material";
+export default function Homepage(){
+    return(
+        <>
+        <Typography>Hello from Homepage</Typography>
+        </>
+    )
+}
